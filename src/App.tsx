fe2d30aec9b2136959b3fe6c7367e15c9fca4576
Jose-MonteFJr <zemfj11@gmail.com>
2026-04-22 10:44:17 -0300

    Icon and Text components done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+
+import Icon from "./components/icon";
 
 export default function App() {
-	return <Text>Ola mundo</Text>;
+	return (
+		<div className="grid gap-3">
+			<div className="flex flex-col gap-2">
+				<Text variant="body-md-bold" className="text-pink-dark">
+					Ola mundo
+				</Text>
+
+				<Text className="text-pink-light">Ola mundo</Text>
+
+				<Text variant="body-sm-bold" className="text-gray-300">
+					Ola mundo
+				</Text>
+
+				<Text>Levar o dog para passear</Text>
+			</div>
+
+			<div className="flex gap-1">
+				<Icon svg={TrashIcon} className="fill-pink-dark" />
+				<Icon svg={CheckIcon} className="fill-pink-dark" />
+				<Icon svg={PencilIcon} className="fill-pink-dark" />
+				<Icon svg={PlusIcon} className="fill-pink-dark" />
+				<Icon svg={XIcon} className="fill-pink-dark" />
+				<Icon svg={SpinnerIcon} animate />
+			</div>
+		</div>
+	);
 }
--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,6 +1,21 @@
+/* eslint-disable react-refresh/only-export-components */
+import { cva, type VariantProps } from "class-variance-authority";
 import React from "react";
 
-interface TextProps {
+export const textVariants = cva("font-sans text-gray-400", {
+	variants: {
+		variant: {
+			"body-sm-bold": "text-sm leading-5 font-semibold",
+			"body-md": "text-base leading-6 font-normal",
+			"body-md-bold": "text-base leading-6 font-semibold",
+		},
+	},
+	defaultVariants: {
+		variant: "body-md",
+	},
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
 	as?: keyof React.JSX.IntrinsicElements;
 	className?: string;
 	children?: React.ReactNode;
@@ -8,6 +23,7 @@ interface TextProps {
 
 export default function Text({
 	as = "span",
+	variant,
 	className,
 	children,
 	...props
@@ -15,7 +31,7 @@ export default function Text({
 	return React.createElement(
 		as,
 		{
-			className,
+			className: textVariants({ variant, className }),
 			...props,
 		},
 		children,
